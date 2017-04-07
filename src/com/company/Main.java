6a3c952012cdc6566717518b29ccb66f219aa129
sleitor <Sleitor@list.ru>
2017-04-07 09:50:48 +0300

    Working Deadlock

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,105 +1,46 @@
 package com.company;
 
+
 /*
-1. Напишите программу, которая каждую секунду отображает на экране данные о времени,
-прошедшем от начала сессии, а другой ее поток выводит сообщение каждые 5 секунд.
-Предусмотрите возможность ежесекундного оповещения потока, воспроизводящего сообщение,
-потоком, отсчитывающим время.
-2. Не внося изменений в код потока-"хронометра" , добавьте еще один поток,
-который выводит на экран другое сообщение каждые 7 секунд. Предполагается
-использование методов wait(), notifyAll().
 */
 
 import static java.lang.Thread.sleep;
 
 public class Main {
-    Object shared = new Object();
 
-    static int x = 1;
+    private final static Object one=new Object(), two=new Object();
 
     public static void main(String[] args) {
 	// write your code here
-/*
-        Theads thead1 = new Theads();
-        Thread Thread1 = new Thread(thead1);
-        Thread1.start();
-        try {
-            thead1.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    Thread t1 = new Thread(){
+        @Override
+        public void run() {
+            synchronized (one){
+                try {
+                    sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (two){
+                    System.out.println(Thread.currentThread().getName() +" Success!");
+                }
+            }
         }
-*/
-        //Создание потока
-        Thread thread1 = new Thread(new Runnable()
-        {
-            public synchronized void run() //Этот метод будет выполняться в побочном потоке
-            {
-                long start = System.currentTimeMillis();
-                for (int i = 0; i < 30; i++) {
+    };
 
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        Thread t2 = new Thread(){
+            @Override
+            public void run() {
+                synchronized (two){
+                    Thread.yield();
+                    synchronized (one){
+                        System.out.println(Thread.currentThread().getName() +" Success!");
                     }
-                    x++;
-                    System.out.println("Count(" + (x-1) +") Привет из первого потока! " + (System.currentTimeMillis()-start));
-                    thead2.notifyAll();
-
-                    //msgQueue.hasMessages();
-
-                }
-
-            }
-
-        });
-        thread1.start();	//Запуск потока
-
-        //Создание потока
-        final Thread thead2 = new Thread(new Runnable()
-        {
-            public synchronized void run() //Этот метод будет выполняться в побочном потоке
-            {
-                while (true) {
-/*
-                    try {
-                        if ((x%5)==0) {
-                            Thread.yield();
-                        }
-                        else
-
-                        if (x==30) return;
-
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-*/                  if (/*(x%5)==0*/true) {
-                        System.out.println("Привет из второго потока!");
-                        try {
-                            this.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (x==30) return;
                 }
             }
-        });
-        thead2.start();	//Запуск потока
-        //Создание потока
-/*
-        Thread thead3 = new Thread(new Runnable()
-        {
-            public void run() //Этот метод будет выполняться в побочном потоке
-            {
-                System.out.println("Привет из побочного потока!");
-            }
-        });
-        thead3.start();	//Запуск потока
-/**/
-        System.out.println("Главный поток завершён...");
+        };
 
-}
+        t1.start();
+        t2.start();
+    }
 }
