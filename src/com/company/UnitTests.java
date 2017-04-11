7341d457c4e56088c39a16db87a8ae43e5b4e8cf
sleitor <Sleitor@list.ru>
2017-04-11 20:32:08 +0300

    Подготовили среду для тестирования, начали писать новый тест

---
+++ src/com/company/UnitTests.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+/**
+ * Created by Троицкий Дмитрий on 11.04.2017.
+ */
+public class UnitTests {
+
+    @Test
+    public void creationResource() {
+
+        Resource resource[] = new Resource[2];
+
+        resource[1] = new Resource( "0", new File("src\\com\\company\\Resources\\file0"));
+
+
+        }
+
+//        Reader reader = new Reader(  )
+    }
+}
