a887e808f0e51efc8565a43b1647f27f31fc6bfc
sleitor <Sleitor@list.ru>
2017-04-17 14:06:09 +0300

    Initial commit

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,7 +1,80 @@
 package com.company;
 
+import com.sun.corba.se.spi.activation.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by Троицкий Дмитрий on 17.04.2017.
  */
 public class Client {
+
+    public static void main(String[] args) {
+
+        Thread Server2 = new Thread( new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+ //                   Socket socket = new Socket( "localhost", 5555 );
+                    Socket socket = Main.serverSocket.accept();
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader( socket.getInputStream() )
+                    );
+
+
+                    while (true) {
+                        String message = bufferedReader.readLine();
+                        if (message != null) {
+                            System.out.println( message );
+                        }
+                    }
+
+
+                  //  socket.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+        );
+
+        Thread Client2 = new Thread( new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Socket socket = new Socket( "localhost", 5555 );
+                    BufferedWriter bufferedWriter = new BufferedWriter(
+                            new OutputStreamWriter( socket.getOutputStream() )
+                    );
+
+                    //                    bufferedWriter.write( "Hello from client 2" );
+
+                    Scanner in = new Scanner(System.in);
+
+
+                    for(int i=0;i < 5; i++){
+                        bufferedWriter.write( "Client: "+ in.next()+ "\n" );
+                        bufferedWriter.flush();
+                    }
+
+                    socket.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } );
+
+        Server2.start();
+//        Client2.start();
+
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,85 @@ package com.company;
 Текст задачи
 */
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Main {
 
+    static ServerSocket serverSocket;
+
+    static {
+        try {
+            serverSocket = new ServerSocket( 5555 );
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+
     public static void main(String[] args) {
+
+//        ServerSocket serverSocket = new ServerSocket( 5555 );
+
+
+        Thread Server = new Thread( new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    Socket socket = serverSocket.accept();
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader( socket.getInputStream() )
+                    );
+
+                    while (true) {
+                        String message = bufferedReader.readLine();
+                        if (message != null) {
+                            System.out.println( message );
+                        }
+                    }
+
+
+                    //socket.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+        );
+
+        Thread Client = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket( "localhost", 5555 );
+                    BufferedWriter bufferedWriter = new BufferedWriter(
+                            new OutputStreamWriter( socket.getOutputStream() )
+                    );
+
+                    Scanner in = new Scanner(System.in);
+
+                    for(int i=0;i < 5; i++){
+                        bufferedWriter.write( "Main: "+ in.next()+ "\n" );
+                        bufferedWriter.flush();
+                    }
+
+                    socket.close();
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } );
+
+        Client.start();
+//        Server.start();
 
     }
 }
